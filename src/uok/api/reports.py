cd42f4e506b7f9811ab693d75b1fe12098b6ee74
Soyuz-Tec <web.soyuztec@gmail.com>
2026-07-09 10:34:58 -0400

    Remove unused reports API wrapper

--- src/uok/api/reports.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from ..module_paths import ensure_module_backend_paths
-
-ensure_module_backend_paths()
-
-from uok_reports_core.api import router  # noqa: E402
-
-__all__ = ["router"]
